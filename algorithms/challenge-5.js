6d0928188a9af6bf5ad6445068b5a94769a744e6
aliyunafiu <aliyunafiu15@yahoo.com>
2022-11-06 23:21:44 +0100

    this is my algorithm assignment

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,15 @@
-function filterList(arr) {
+function filterList (arr); {
+ const filterFigues =(arr) =>{
+    return arr.filter((item)=> {
+        return typeof item === "number";
+    }
+    console.log(filterFigues);
+
  
- }
+
 
   module.exports =filterList
+
+  //filterList([1,2,'a','b']) to return [1,2]
+ // filterList([1,'a','b',0,15]) to return [1,0,15]
+ // filterList([1,2,'aasf','1','123',123]) to return [1,2,123]
