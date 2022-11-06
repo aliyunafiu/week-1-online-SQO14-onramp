365a3f92d8c5e5c2e04921cd83291e2959df68e9
aliyunafiu <aliyunafiu15@yahoo.com>
2022-11-06 23:17:13 +0100

    this is my algorithm assignment

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -8,6 +8,3 @@ const count2 = arr2.filter(Boolean).length;
 console.log(count2);
 module.exports = countTruthy
 
-//countTruthy([6, 3, 0, 30, 7])  to return 4 
-//countTruthy(['', 3, 0, 30, 7]) to return 3
-//countTruthy(['', 0, 0, undefined, 7])  to return 1
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -14,8 +14,3 @@ function fizzBuzz(input){
 
 
 module.exports = fizzBuzz
-//Implement a function fizzBuzz(input) that takes a number and retuns:
-//"fizz": if the number is divisible by 3
-//"buzz": if the number is divisible by 5
-//"fizzbuzz": if the number is both divisible by 3 and 5
-//number: if it is not divisible by 3 or 5
